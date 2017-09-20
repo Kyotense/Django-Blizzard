6e14727db867dd3aca6ff351dfa6e9e689bcb216
Kyotense <makoto_singleton@hotmail.com>
2017-09-20 09:03:29 -0700

    terrible sticky navbar added. Functionally works, navbar looks terrible. Need to make it look pretty.

---
+++ mysite/main/static/javascript/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  var stickyNavTop = $(".nav-header").offset().top;
+
+  var stickyNav = function(){
+    var scrollTop = $(window).scrollTop();
+
+    if(scrollTop > stickyNavTop){
+      $(".nav-header").addClass("sticky");
+    }
+    else{
+      $(".nav-header").removeClass("sticky")
+    }
+  };
+  stickyNav()
+  $(window).scroll(function(){
+    stickyNav();
+  });
+});
