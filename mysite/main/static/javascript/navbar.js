9561364f49f946ee866a61ca0718d8eca8de9bcd
Kyotense <makoto_singleton@hotmail.com>
2017-09-22 13:35:21 -0700

    The navbar now slides down when you scroll down.

--- mysite/main/static/javascript/navbar.js
+++ mysite/main/static/javascript/navbar.js
@@ -1,18 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function() {
   var stickyNavTop = $(".container").offset().top;
 
-  var stickyNav = function(){
+  var stickyNav = function() {
     var scrollTop = $(window).scrollTop();
 
-    if(scrollTop > stickyNavTop){
-      $(".container").addClass("sticky");
-    }
-    else{
+    if (scrollTop > stickyNavTop) {
+      $(".container").stop().addClass("sticky").css("top", "-50px").animate({
+        'top': "0px"
+      }, 500);
+    } else {
       $(".container").removeClass("sticky");
     }
   };
   stickyNav()
-  $(window).scroll(function(){
+  $(window).scroll(function() {
     stickyNav();
   });
 
