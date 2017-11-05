d3794ce4a2c5bfc95933bc858356de8435332f3e
Kyotense <makoto_singleton@hotmail.com>
2017-11-05 13:31:34 -0800

    Starting the body

--- mysite/main/static/javascript/navbar2.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function(e){
-    if ($(this).scrollTop() > 30) { // choose the value you want.
-        $('.container:hidden').slideDown();
-    } else {
-        $('.container:visible').slideUp();
-    }
-});
